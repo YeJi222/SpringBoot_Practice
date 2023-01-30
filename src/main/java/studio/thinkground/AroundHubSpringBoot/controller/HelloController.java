bf8636a58c0e801c2bfd8507e52d72734b857c28
YeJi222 <70511859+YeJi222@users.noreply.github.com>
2023-01-30 16:22:14 +0900

    feat: edit HelloController.java to use LOGGER

--- src/main/java/studio/thinkground/AroundHubSpringBoot/controller/HelloController.java
+++ src/main/java/studio/thinkground/AroundHubSpringBoot/controller/HelloController.java
@@ -1,15 +1,31 @@
 package studio.thinkground.AroundHubSpringBoot.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloController {
-    // @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    private final Logger LOGGER = LoggerFactory.getLogger(HelloController.class);
+
+    @RequestMapping("/hello")
+    public String hello(){
+        return "Hello World!";
+    }
+
+    /*
     @GetMapping("hello")
     public String hello(){
         return "Hello World!";
+    }
+    */
+
+    @PostMapping("log-test")
+    public void logTest(){
+        LOGGER.trace("Trace Log");
+        LOGGER.debug("Debug Log");
+        LOGGER.info("Info Log");
+        LOGGER.warn("Warn Log");
+        LOGGER.error("Error Log");
     }
 }

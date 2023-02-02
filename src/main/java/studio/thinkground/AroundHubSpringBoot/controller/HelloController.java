55ec7588af9129f3d5de0cfeb46536f18580269a
YeJi222 <70511859+YeJi222@users.noreply.github.com>
2023-02-02 15:01:47 +0900

    feat: add exception code

--- src/main/java/studio/thinkground/AroundHubSpringBoot/controller/HelloController.java
+++ src/main/java/studio/thinkground/AroundHubSpringBoot/controller/HelloController.java
@@ -2,7 +2,16 @@ package studio.thinkground.AroundHubSpringBoot.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class HelloController {
@@ -27,5 +36,26 @@ public class HelloController {
         LOGGER.info("Info Log");
         LOGGER.warn("Warn Log");
         LOGGER.error("Error Log");
+    }
+
+    @PostMapping("/exception")
+    public void exceptionTest() throws Exception{
+        throw new Exception();
+    }
+
+    @ExceptionHandler(value = Exception.class)
+    public ResponseEntity<Map<String, String>> ExceptionHandler(Exception e){
+        HttpHeaders responseHeaders = new HttpHeaders();
+        HttpStatus httpStatus = HttpStatus.BAD_REQUEST;
+
+        LOGGER.info(e.getLocalizedMessage());
+        LOGGER.info("Controller 내 ExceptionHandler 호출");
+
+        Map<String, String> map = new HashMap<>();
+        map.put("error type", httpStatus.getReasonPhrase());
+        map.put("code", "400");
+        map.put("message", "에러 발생");
+
+        return new ResponseEntity<>(map, responseHeaders, httpStatus);
     }
 }

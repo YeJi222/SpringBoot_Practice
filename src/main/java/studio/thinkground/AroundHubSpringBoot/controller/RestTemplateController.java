b33854442ee03acd018aa06c6b5ff7c1fd29a7cd
YeJi222 <70511859+YeJi222@users.noreply.github.com>
2023-02-03 23:48:26 +0900

    feat: add RestTemplateService & RestTemplateServiceImpl

--- src/main/java/studio/thinkground/AroundHubSpringBoot/controller/RestTemplateController.java
+++ src/main/java/studio/thinkground/AroundHubSpringBoot/controller/RestTemplateController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import studio.thinkground.AroundHubSpringBoot.dto.MemberDTO;
+import studio.thinkground.AroundHubSpringBoot.service.RestTemplateService;
 
 @RestController
 @RequestMapping("/api/rest-template")
